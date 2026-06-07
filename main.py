e6b46f5cf413178df7f3f405affb3a1e52a0caf3
timur-manjosov <timurmanjosov.dev@proton.me>
2026-06-07 13:04:35 +0200

    feat: add replay option after each round

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import random
-import subprocess
 
-def clear_console():
-    subprocess.call("clear")
+def play_game():
+    random_zahl = random.randint(1, 10)
+    print("Ich habe eine Zahl zwischen 1 bis 10 ausgesucht. Raten sie, welche es ist!\n")
 
-random_zahl = random.randint(1, 10)
-print("Ich habe eine Zahl zwischen 1 bis 10 ausgesucht. Raten sie, welche es ist!\n")
+    while True:
+        x = input()
+        try:
+            x = int(x)
 
-while True:
-    x = input()
-    try:
-        x = int(x)
+            if (x == random_zahl):
+                print("Herzlichen Glückwunsch!")
+                break
+            else:
+                if (x < random_zahl):
+                    print("Die Schätzung ist kleiner als die zu erratende Zahl!\n")
+                elif (x > random_zahl):
+                    print("Die Schätzung ist größer als die zu erratende Zahl!\n") 
 
-        if (x == random_zahl):
-            print("Herzlichen Glückwunsch!")
-            break
-        else:
-            if (x < random_zahl):
-                print("Die Schätzung ist kleiner als die zu erratende Zahl!\n")
-                
-            elif (x > random_zahl):
-                print("Die Schätzung ist größer als die zu erratende Zahl!\n")
-                
+        except ValueError:
+            print("Bitte geben sie eine Zahl ein!\n")
 
-    except ValueError:
-        print("Bitte geben sie eine Zahl ein!\n")
+def start_service():
+    answer = input("You want to quit? Type n to quit the game. Otherweise continue with any letter!\n")
+
+    while (answer != "n"):
+        play_game()
+        answer = input("You want to quit? Type n to quit the game. Otherweise continue with any letter!\n")
+
+
+    print("Thanks for playing!")
+
+
+start_service()
